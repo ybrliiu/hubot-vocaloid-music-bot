bdd0405c4c90289997fb8d23002bd659d8004c18
mp0liiu <raian@reeshome.org>
2019-04-16 20:48:23 +0900

    最初の動画を取ってくるように

--- src/vocahito-nippo.ts
+++ src/vocahito-nippo.ts
@@ -1,5 +1,6 @@
 import { Robot, Response } from 'hubot';
 import Axios from 'axios';
+import { JSDOM } from 'jsdom';
 
 module.exports = (robot: Robot<any>) => {
 
@@ -14,7 +15,19 @@ async function fetchMoviesURL(): Promise<string> {
   const urls =
     await Axios.get('http://cobachican.hatenadiary.jp/')
       .then(response => {
-        return response.data;
+        const dom = new JSDOM(response.data);
+        const out = dom.window.document.getElementsByTagName('iframe');
+        if ( out !== null ) {
+          const item = out.item(1);
+          if ( item !== null ) {
+            const src = item.getAttribute('src');
+            if ( src !== null ) {
+              const splited = src.split('/');
+              return 'https://www.nicovideo.jp/watch/' + splited[ splited.length - 1 ];
+            }
+          }
+        }
+        return 'nothing...';
       })
       .catch(error => { return error });
 
