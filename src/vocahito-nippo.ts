64b63dc04791bbb29671a601eb7a5fe622fabc0b
mp0liiu <raian@reeshome.org>
2019-06-17 23:59:21 +0900

    channelIDを指定すればRobot.sendでメッセージを送信できる

--- src/vocahito-nippo.ts
+++ src/vocahito-nippo.ts
@@ -1,27 +1,47 @@
 import { Robot, Response } from 'hubot';
 import Axios from 'axios';
 import { JSDOM } from 'jsdom';
-import * as cron from 'node-cron';
+// import * as cron from 'node-cron';
+
+interface Log {
+  error(message: string): void;
+}
 
 declare module 'hubot' {
+
+  type Envelope = {
+    /* room is channelID. 
+     * How to get discord channelID -> 
+     * https://github.com/Chikachi/DiscordIntegration/wiki/How-to-get-a-token-and-channel-ID-for-Discord
+     */
+    room: string
+  };
+
   interface Robot<A> {
-    send(envelope: { room: string }, ...strings: string[]): void;
+
+    logger: Log;
+
+    send(envelope: Envelope, ...strings: string[]): void;
+
   }
+
 }
 
 module.exports = (robot: Robot<any>) => {
 
-  cron.schedule('* * * * *', () => {
-    robot.send({ room: 'vocaloid-music' }, 'てすと');
-  });
+  const channelID = process.env.DISCORD_CHANNEL_ID;
+  if ( channelID !== undefined ) {
+    robot.send({ room: channelID }, 'てすと');
+  } else {
+    robot.logger.error('環境変数 DISCORD_CHANNEL_ID が設定されていないので、botを動作させられません.');
+  }
 
   robot.hear(/ボカヒト日報見て/, (response: Response<Robot<any>>) => {
     fetchMoviesURL()
       .then(urls => {
         if ( urls.length === 0 ) {
           response.send('本日のボカロ曲はないみたいですね・・・。');
-        }
-        else {
+        } else {
           response.send(urls);
         }
       })
@@ -43,13 +63,11 @@ async function fetchMoviesURL(): Promise<string> {
             if ( src !== null ) {
               const splited = src.split('/');
               return 'https://www.nicovideo.jp/watch/' + splited[ splited.length - 1 ];
-            }
-            else {
+            } else {
               return 'Parse error ocured.';
             }
           }]).join("\n");
-        }
-        else {
+        } else {
           return 'There are no articles.'
         }
       })
